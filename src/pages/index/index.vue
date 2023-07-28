6e1c94b3759f4f6b9c9c04963622b44331569089
wangchangqin <changqin.wang@rokid.com>
2023-07-28 18:21:50 +0800

    新增index页面

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,18 +1,41 @@
 <template>
   <view class="index">
-    <text>{{ msg }}</text>
+    <thread-list :threads="threads" :loading="loading" />
   </view>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { reactive, onMounted } from 'vue'
+import Taro from '@tarojs/taro'
+import api from '../../utils/api'
+import ThreadList from '../../components/thread_list.vue'
 import './index.less'
 
 export default {
+  components: {
+    'thread-list': ThreadList,
+  },
   setup () {
-    const msg = ref('Hello world')
+    const loading = reactive(true)
+    const threads = reactive([])
+
+    onMounted(async () => {
+      try {
+        const res = await Taro.request({
+          url: api.getLatestTopic(),
+        })
+        loading.value = false
+        threads.value = res.data
+      } catch (error) {
+        Taro.showToast({
+          title: '载入远程数据错误',
+        })
+      }
+    })
+
     return {
-      msg
+      loading,
+      threads,
     }
   }
 }
